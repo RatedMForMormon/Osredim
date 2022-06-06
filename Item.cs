0ad7bbc77480709b2d9dff828956f6021cee254f
RatedMForMormon <83249275+RatedMForMormon@users.noreply.github.com>
2022-06-06 11:15:31 -0600

    Fixed Constructor, added typename

--- Item.cs
+++ Item.cs
@@ -3,15 +3,32 @@ using Unity;
 
 Class Item(Monobehavior)
   {
-    string name = new string();
-    char type = new char();
-    Dictionary attributes = new Dictionary();
-    Dictionary effects = new Dictionary();
-    item(string name, char type, Dictionary attributes, Dictionary effects){
-      this.name = name;
-      this.type = type;
-      this.attributes = attributes;
-      this.effects = effects;
+    string itemName = new string();
+    char itemType = new char();
+    Dictionary itemAttributes = new Dictionary();
+    Dictionary itemEffects = new Dictionary();
+    string typeName = new string();
+  
+    Item(string name, char type, Dictionary attributes, Dictionary effects){
+      itemName = name;
+      itemType = type;
+      itemAttributes = attributes;
+      itemEffects = effects;
+      
+    }
+    
+    public string getName(){
+      return itemName;
+    }
+    public string getType(char type){
+      switch(type){
+        case 'r':
+          return "ring";
+        case 'a':
+          return "armor";
+        case 'w':
+          return "weapon";
+      }
     }
     
   }
