c60ade901ca6f3c3b91e62ead906e9f4b1844dde
RatedMForMormon <83249275+RatedMForMormon@users.noreply.github.com>
2022-09-05 10:02:06 -0600

    Update Npc.cs

--- Npc.cs
+++ Npc.cs
@@ -2,8 +2,21 @@ using System;
 using Unity;
 
 public Class Npc(): Monobehavior {
-  int favor;
-  int hp;
-  int mana;
-  int stamina;
+  
+  private int favor; // Favor tells the NPC how much it likes you, -100 is kill on sight and 100 is will die for you
+  private int hp;
+  private int mana;
+  private int stamina;
+  private string name;
+  
+  public Npc(int favor, int hp, int mana, int stamina, string name){
+    this.favor = favor
+    this.hp = hp
+    this.mana = mana 
+    this.stamina = stamina
+    this.name = name
+  }
+  
+  
+  
 }
